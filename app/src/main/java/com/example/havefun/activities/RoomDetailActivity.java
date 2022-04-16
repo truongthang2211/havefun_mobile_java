868d79e060f77e54fdcdfe2e37a27edf9a654bae
Beater1804 <thanhdung12qh@gmail.com>
2022-04-16 15:00:35 +0700

    detail_room by dungnt

--- app/src/main/java/com/example/havefun/activities/RoomDetailActivity.java
+++ app/src/main/java/com/example/havefun/activities/RoomDetailActivity.java
@@ -1,10 +1,14 @@
 package com.example.havefun.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
 
 import android.os.Bundle;
 
+
 import com.example.havefun.R;
+import com.example.havefun.adapters.ImageAdapter;
 
 public class RoomDetailActivity extends AppCompatActivity {
 
@@ -12,5 +16,9 @@ public class RoomDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room_detail);
+
+        ViewPager viewPager = findViewById(R.id.imgViewPager);
+        ImageAdapter imageAdapter = new ImageAdapter(this);
+        viewPager.setAdapter(imageAdapter);
     }
 }
---
+++ app/src/main/java/com/example/havefun/adapters/ImageAdapter.java
@@ -0,0 +1,51 @@
+package com.example.havefun.adapters;
+
+import android.content.Context;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.havefun.R;
+
+
+public class ImageAdapter extends PagerAdapter {
+    Context context;
+
+    public ImageAdapter(Context context){
+        this.context = context;
+    }
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == ((ImageView) object);
+    }
+
+    private int[] sliderImageId = new int[]{
+            R.drawable.image1, R.drawable.image2, R.drawable.image3,R.drawable.image4, R.drawable.image5,
+    };
+
+    @NonNull
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        ImageView imageView = new ImageView(context);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        imageView.setImageResource(sliderImageId[position]);
+        ((ViewPager) container).addView(imageView, 0);
+        return imageView;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        ((ViewPager) container).removeView((ImageView) object);
+    }
+
+    @Override
+    public int getCount() {
+        return sliderImageId.length;
+    }
+}
