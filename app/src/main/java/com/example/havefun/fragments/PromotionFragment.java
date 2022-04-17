77429a59f953d163447dadd09921b77b79f0fa4a
truongthang2211 <truongthang2211@gmail.com>
2022-04-17 13:44:34 +0700

    intent detail promotion

--- app/src/main/java/com/example/havefun/fragments/PromotionFragment.java
+++ app/src/main/java/com/example/havefun/fragments/PromotionFragment.java
@@ -1,5 +1,6 @@
 package com.example.havefun.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,9 +8,12 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.havefun.R;
+import com.example.havefun.activities.DetailPromotionActivity;
 import com.example.havefun.databinding.FragmentPromotionBinding;
 import com.example.havefun.viewmodels.PromotionViewModel;
 
@@ -28,6 +32,19 @@ public class PromotionFragment extends Fragment {
 //        final TextView textView = binding.textDashboard;
 //        promotionViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        View pro_item = view.findViewById(R.id.pro_item_promote_incl);
+        pro_item.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(getActivity(), DetailPromotionActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
